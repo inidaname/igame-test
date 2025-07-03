cd1901d3b8527265f86d2c626522888ba536e4a8
Hassan Sani <saniyhassan@gmail.com>
2025-07-03 22:58:26 +0100

    feat: restructured directory

--- src/types/components.d.ts
+++
@@ -1,9 +0,0 @@
-type ButtonProps = React.DetailedHTMLProps<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->;
-
-type InputProps = React.DetailedHTMLProps<
-  React.InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElementt
->;
